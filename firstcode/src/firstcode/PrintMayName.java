b348c25192d840f422e30013f7bb6d178fc8535b
acer <acer@DESKTOP-436QC9F>
2020-02-15 20:50:46 +0300

    first code

---
+++ firstcode/src/firstcode/PrintMayName.java
@@ -0,0 +1,10 @@
+package firstcode;
+
+public class PrintMayName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("My name is hussam");
+	}
+
+}
